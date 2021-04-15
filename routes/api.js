76300199eda7219b3b3cd13ca7e32baa775636ac
Alex Marzullo <marzullo94@gmail.com>
2021-04-15 12:34:28 -0400

    adding api.js to routes to route data to backend

--- routes/api.js
+++ routes/api.js
@@ -1,3 +1,78 @@
 const router = require("express").Router();
-const Exercise = require("../models/exercise")
+const Exercise = require("../models/exercise.js")
 
+router.post('/api/exercise', (req, res) => {
+    Exercise.create({})
+    .then((dbExercise) => {
+      res.json(dbExercise);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.put('/api/exercise/:id', ({ body, params }, res) => {
+    Exercise.findByIdAndUpdate(
+    params.id,
+    { $push: { exercises: body } },
+    // "runValidators" will ensure new exercises meet our schema requirements
+    { new: true, runValidators: true }
+  )
+    .then((dbExercise) => {
+      res.json(dbExercise);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.get('/api/exercise', (req, res) => {
+    Exercise.aggregate([
+    {
+      $addFields: {
+        totalDuration: {
+          $sum: '$exercises.duration',
+        },
+      },
+    },
+  ])
+    .then((dbExercises) => {
+      res.json(dbExercises);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.get('/api/exercise/range', (req, res) => {
+    Exercise.aggregate([
+    {
+      $addFields: {
+        totalDuration: {
+          $sum: '$exercises.duration',
+        },
+      },
+    },
+  ])
+    .sort({ _id: -1 })
+    .limit(7)
+    .then((dbExercises) => {
+      console.log(dbExercises);
+      res.json(dbExercises);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.delete('/api/exercise', ({ body }, res) => {
+    Exercise.findByIdAndDelete(body.id)
+    .then(() => {
+      res.json(true);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+module.exports = router;
