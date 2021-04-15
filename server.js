638034d8b9d82b87af454723b87913276804412d
Alex Marzullo <marzullo94@gmail.com>
2021-04-15 13:27:25 -0400

    deploying

--- server.js
+++ server.js
@@ -10,7 +10,15 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(logger("dev"));
 
-mongoose.connect("mongodb://localhost/exercise");
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
 
 app.use(require("./routes/api"))
 app.use(require("./routes/html"))
